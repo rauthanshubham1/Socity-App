ef0550f1a8d198d3b628afd94a39068ad9cf6792
Shubham Rauthan <codexshubham@gmail.com>
2023-06-27 20:47:59 +0530

    Beautification of messages feature

--- client/src/components/AllMessages.js
+++ client/src/components/AllMessages.js
@@ -38,18 +38,12 @@ const AllMessages = () => {
         <div className='allMsgs'>
             <Header heading="Your Messages" />
             <div className="inbox">
-                <Inbox />
-                <Inbox />
-                <Inbox />
-                <Inbox />
-                <Inbox />
-                <Inbox />
-                <Inbox />
-                <Inbox />
-                <Inbox />
-                <Inbox />
-                <Inbox />
-                <Inbox />
+                <Inbox name={"User1"} url={"https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png"} />
+                <Inbox name={"User2"} url={"https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png"} />
+                <Inbox name={"User3"} url={"https://w7.pngwing.com/pngs/490/157/png-transparent-male-avatar-boy-face-man-user-flat-classy-users-icon.png"} />
+                <Inbox name={"User4"} url={"https://www.shareicon.net/data/512x512/2016/05/24/770137_man_512x512.png"} />
+                <Inbox name={"User5"} url={"https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png"} />
+                <Inbox name={"User6"} url={"https://cdn.icon-icons.com/icons2/2468/PNG/512/user_kids_avatar_user_profile_icon_149314.png"} />
             </div>
         </div>
 
--- client/src/components/Inbox.js
+++ client/src/components/Inbox.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import "../componentsStyle/Inbox.css"
 
-const Inbox = () => {
+const Inbox = ({ name, url }) => {
     return (
         <div className="message">
             <div className="avatar">
-                <img src="avatar1.jpg" alt="Avatar" />
+                <img src={url} alt="Avatar" />
             </div>
             <div className="content">
-                <h3>John Doe</h3>
+                <h3>{name}</h3>
             </div>
         </div>
     )
