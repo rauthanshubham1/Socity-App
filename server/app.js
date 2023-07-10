9848c6d5bb6c10d7ee3d1aa521a5e3938d0f288a
Shubham Rauthan <codexshubham@gmail.com>
2023-07-10 16:14:00 +0530

    Modified cors function

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({ credentials: true, origin: true, methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" })
 require("./db/connect");
