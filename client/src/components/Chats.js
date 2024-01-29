037efe5b1f32ebd27740b65fd7e12d809ddf5f27
Shubham Rauthan <codexshubham@gmail.com>
2024-01-29 22:27:45 +0530

    Updated socket route

--- client/src/components/Chats.js
+++ client/src/components/Chats.js
@@ -4,7 +4,7 @@ import "../componentsStyle/chats.css"
 import { useLocation, useNavigate } from "react-router-dom"
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:4000")
+const socket = io.connect(process.env.REACT_APP_ROUTE);
 
 const Chats = () => {
     const [textBoxMsg, setTextBoxMsg] = useState("")
